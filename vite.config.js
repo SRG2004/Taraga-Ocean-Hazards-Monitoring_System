26d3e49ffe6c173a04f6d9f1498b346ecfeabe1c
sarthakgosavi77 <sarthakgosavi77@gmail.com>
2025-09-27 15:41:05 +0000

    fix: Resolve style and login issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +10,11 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
   },
   server: {
     port: 5000
